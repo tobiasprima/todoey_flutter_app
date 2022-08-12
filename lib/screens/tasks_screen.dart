4244b808d810d0674f999a99c8a58812d63f492c
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-12 21:54:01 +0700

    Design the App

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -7,52 +7,61 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-                color: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          padding:
+              EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                child: Icon(
+                  Icons.list,
+                  size: 30.0,
+                  color: Colors.lightBlueAccent,
+                ),
+                backgroundColor: Colors.white,
+                radius: 30.0,
               ),
-              backgroundColor: Colors.white,
-              radius: 30.0,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Todoey',
-              style: TextStyle(
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Todoey',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700),
+              ),
+              Text(
+                '12 Tasks',
+                style: TextStyle(
                   color: Colors.white,
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w700),
-            ),
-            Text(
-              '12 Tasks',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              height: 300.0,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
+                  fontSize: 18,
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+        Expanded(
+          child: Container(
+            color: Colors.white,
+            height: 300.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              ),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
