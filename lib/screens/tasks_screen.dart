c05181a7434359741f90efd8394f7e3aad2bf05e
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-12 21:45:59 +0700

    Design the App

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -11,6 +11,7 @@ class TasksScreen extends StatelessWidget {
         padding:
             EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CircleAvatar(
               child: Icon(
@@ -24,7 +25,31 @@ class TasksScreen extends StatelessWidget {
             SizedBox(
               height: 10.0,
             ),
-            Text('Todoey'),
+            Text(
+              'Todoey',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.w700),
+            ),
+            Text(
+              '12 Tasks',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+            ),
           ],
         ),
       ),
