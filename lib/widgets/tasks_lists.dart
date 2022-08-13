50a3bed8f1a21b1df85a9f8ca8128fcdf7668c8e
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-13 21:50:13 +0700

    Add a Add Task Functionality

--- lib/widgets/tasks_lists.dart
+++ lib/widgets/tasks_lists.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/widgets/tasks_tile.dart';
+import 'package:todoey_flutter/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> task = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
