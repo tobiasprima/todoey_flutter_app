916a5161c86cc07a850f424913be20e2354a5d36
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-14 20:18:01 +0700

    Add a Add Task Functionality

--- lib/widgets/tasks_lists.dart
+++ lib/widgets/tasks_lists.dart
@@ -16,21 +16,11 @@ class _TasksListState extends State<TasksList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TasksTile(
-          taskTitle: tasks[0].name,
-          isChecked: tasks[0].isDone,
-        ),
-        TasksTile(
-          taskTitle: tasks[1].name,
-          isChecked: tasks[1].isDone,
-        ),
-        TasksTile(
-          taskTitle: tasks[2].name,
-          isChecked: tasks[2].isDone,
-        ),
-      ],
-    );
+    return ListView.builder(itemBuilder: ((context, index) {
+      return TasksTile(
+        isChecked: tasks[index].isDone,
+        taskTitle: tasks[index].name,
+      );
+    }));
   }
 }
